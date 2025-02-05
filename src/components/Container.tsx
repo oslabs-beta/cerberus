4195bba7df7e81673bcdb988cd8c1075ac9c0639
Molly Josephson <josephson4@yahoo.com>
2025-02-05 12:45:23 -0800

    Co-authored-by: duimaurisfootball <duimaurisfootball@users.noreply.github.com>

---
+++ src/components/Container.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import '../styles/container.css';
+import { useState } from 'react';
+
+const Container = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div className='dropdown'>
+      <h1>Stop worrying and start coding</h1>
+      <h2>
+        Download the ultimate authentication package (currently only available
+        for React + Vite + Node)
+      </h2>
+      <a href='https://github.com/oslabs-beta/cerberus' className='github-repo'>
+        GitHub Repo
+      </a>
+      <h3>
+        Try 3 different login types: Form-based, OAuth, and Passkey
+        authentication
+      </h3>
+      <button onClick={handleOpen}>Choose your method of authentication</button>
+      {open ? (
+        <ul className='menu'>
+          <li className='menu-item'>
+            <button>Form-based</button>
+          </li>
+          <li className='menu-item'>
+            <button>OAuth</button>
+          </li>
+          <li className='menu-item'>
+            <button>Passkey</button>
+          </li>
+        </ul>
+      ) : null}
+    </div>
+  );
+};
+
+export default Container;
